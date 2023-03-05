ea3a9d9c72d3b9e514d3a9ae3f592f5c32aa6ef4
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-03-06 00:49:30 +0900

    상품 스타일 적용
    module.css 이용해서 상품 정보 스타일 적용

--- my-study/src/components/ItemList.js
+++ my-study/src/components/ItemList.js
@@ -1,14 +1,18 @@
 import { Grid } from "semantic-ui-react";
+import styles from './ItemList.module.css';
 
 export default function ItemList({ list }) {
     return (
-        <div>
+        <div className={styles.wrap}>
             <Grid columns={3}>
                 <Grid.Row>
                     {list.map((item) => (
 
                         <Grid.Column>
-                            <img src={item.image_link} alt={item.name} />
+                            <img src={item.image_link} alt={item.name} className={styles.img_item}/>
+                            <strong className={styles.img_item}>{item.name}</strong>
+                            <span className={styles.txt_info}>{item.category} {item.product_type}</span>
+                            <strong>${item.price}</strong>
                         </Grid.Column>
 
                     ))}
